0d3642d1279e3d535dbacb137b405ef82c1b8496
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2021-09-01 20:27:13 +0300

    Initial Commit

---
+++ NewsByGN/NewsByGNApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsByGNApp.swift
+//  NewsByGN
+//
+//  Created by Ivars Ruģelis on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsByGNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
